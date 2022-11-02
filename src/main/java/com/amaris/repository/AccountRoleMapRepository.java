33613e420a269c96f4e5425fe801c0e47d010de0
LE Nguyen Khang <nguyenkhang.le@amaris.com>
2022-11-02 09:29:33 +0700

    fix bug sql

--- src/main/java/com/amaris/repository/AccountRoleMapRepository.java
+++ src/main/java/com/amaris/repository/AccountRoleMapRepository.java
@@ -4,6 +4,7 @@ import com.amaris.domain.AccountRoleMap;
 import com.amaris.domain.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,7 +15,7 @@ public interface AccountRoleMapRepository extends JpaRepository<AccountRoleMap, 
     List<AccountRoleMap> findAllByAccountId(int accountId);
 
     @Query(value = "SELECT userRole FROM AccountRoleMap AS accountRoleMap " +
-                   "INNER JOIN UserRole AS userRole ON accountRoleMap.roleId = userRole.id " +
-                   "WHERE accountRoleMap.accountId = :accountId")
-    List<UserRole> findAllRoleByAccountId(int accountId);
+                   " INNER JOIN UserRole AS userRole ON accountRoleMap.roleId = userRole.id " +
+                   " WHERE accountRoleMap.accountId = :accountId ")
+    List<UserRole> findAllRoleByAccountId(@Param("accountId") int accountId);
 }
